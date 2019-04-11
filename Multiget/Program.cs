38d1f48064c9be1a4d3066665ddc76e05e9fd7b5
Adrian Sims <adriansims62781@gmail.com>
2019-04-11 22:00:34 +0200

    Add project files.

---
+++ Multiget/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiget
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception($"You must specify a file of URLs.");
+                }
+
+                var urls = new List<string>();
+                foreach (var line in File.ReadLines(args[0]).Where(x=>!string.IsNullOrEmpty(x)))
+                {
+                    urls.Add(line);
+                }
+
+                var getter = new UrlGetter(urls);
+                await getter.GetAll();
+            }
+            catch (Exception ex)
+            {
+                var fullname = System.Reflection.Assembly.GetEntryAssembly().Location;
+                var progname = Path.GetFileNameWithoutExtension(fullname);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Multiget/UrlGetter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Multiget
+{
+    internal class UrlGetter
+    {
+        static int filecounter = 0;
+        private readonly IEnumerable<string> urls;
+
+        public UrlGetter(IEnumerable<string> urls)
+        {
+            this.urls = urls;
+        }
+
+        private async Task GetSingleUrl(WebRequest req)
+        {
+            filecounter++;
+            using (var outputStream = File.CreateText($"gtest-{filecounter:D3}.jpg"))
+            using (var response = req.GetResponse())
+            using (var webStream = response.GetResponseStream())
+            {
+                await webStream.CopyToAsync(outputStream.BaseStream);
+            }
+        }
+
+        internal async Task GetAll()
+        {
+            foreach (var url in urls)
+            {
+                var request = WebRequest.Create(url);
+                await GetSingleUrl(request);
+            }
+        }
+    }
+}
